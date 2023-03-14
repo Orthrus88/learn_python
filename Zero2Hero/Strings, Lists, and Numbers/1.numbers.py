48dfc9d88e42d81984812dfcc6b834663c81c5b8
Orthrus88 <123018169+Orthrus88@users.noreply.github.com>
2023-03-14 10:33:39 -0500

    updating format and finalizing the strings lists and numbers seciton

---
+++ Zero2Hero/Strings, Lists, and Numbers/1.numbers.py
@@ -0,0 +1,19 @@
+# Working with numbers in python
+
+# Python as a calculator
+'''
++ addition
+- subraction
+* multiplaction
+/ division
+% modulo or mod - Gives you the remainder of a division so 7 % 4 would return 3 as 4 goes into 7 one time with a remainder of 3
+** to the power
+'''
+
+print(2+1) # returns 3 or 2 + 1
+print(2-1) # returns 1 or 2 - 1
+print(2*2) # returns 4 or 2 times 2
+print(3/2) # returns 1.5 or 3 devided by 2
+print(3%2) # returns 1 or 3 mod 2 as the remainder of 3 devided by 2 is 1
+print(2**4) # returns 16 or 2 to the power of 4
+
