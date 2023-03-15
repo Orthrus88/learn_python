61278b0620870e1a2ad6c359974ec07a770c1a38
ntagain <114170763+ntagain@users.noreply.github.com>
2023-03-14 22:02:08 -0500

    Create 1-3.challenge.answers.py
    answer 1 submitted!

---
+++ Zero2Hero/Challenge/1-3.challenge.answers.py
@@ -0,0 +1,11 @@
+"""
+Answer these 3 questions.
+    What is the value of the expression 4 * (6 + 5)
+    What is the value of the expression 4 * 6 + 5 
+    What is the value of the expression 4 + 6 * 5
+
+Solution:
+"""
+print(4 * (6 + 5))
+print(4 * 6 + 5)
+print(4 + 6 *5)
