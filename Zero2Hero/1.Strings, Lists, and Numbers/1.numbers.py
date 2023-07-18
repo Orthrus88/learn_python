a7b0b5797df7a30db0bdabaf1c636db6cc814d30
Orthrus88 <123018169+Orthrus88@users.noreply.github.com>
2023-07-18 18:46:25 -0500

    updates

--- Zero2Hero/1.Strings, Lists, and Numbers/1.numbers.py
+++ Zero2Hero/1.Strings, Lists, and Numbers/1.numbers.py
@@ -9,10 +9,27 @@
 % modulo or mod - Gives you the remainder of a division so 7 % 4 would return 3 as 4 goes into 7 one time with a remainder of 3
 ** to the power
 '''
-
+print('addition')
 print(2+1) # returns 3 or 2 + 1
+print('')
+print('subtraction')
 print(2-1) # returns 1 or 2 - 1
+print('')
+print('multiplication')
 print(2*2) # returns 4 or 2 times 2
+print('')
+print('division')
 print(3/2) # returns 1.5 or 3 devided by 2
+print('')
+print('Modulo or mod')
 print(3%2) # returns 1 or 3 mod 2 as the remainder of 3 devided by 2 is 1
+print('')
+print('to the power')
 print(2**4) # returns 16 or 2 to the power of 4
+
+# Order of operations
+print('')
+print('OOO')
+print(2 + 10 * 10 + 3)
+#vs
+print((2 + 10) * (10 + 3))
---
+++ Zero2Hero/1.Strings, Lists, and Numbers/2.var_assignments.py
@@ -0,0 +1,13 @@
+# Rules for Variable assignments
+'''
+Can not start with a number
+
+No spaces in a variable
+
+Can't use any of these symbols
+: ' " , < > / ? | \ ( ) ! @ # $ % ^ & * ~ + -
+
+Vars are typically all lower case by best practies except in certain situations
+
+Avoid using words that have special meaning in Python like 'list' and 'str'
+'''
