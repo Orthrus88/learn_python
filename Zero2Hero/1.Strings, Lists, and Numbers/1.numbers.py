38928ef373971eae0453591bb10004dcd0fbcfc6
Orthrus88 <123018169+Orthrus88@users.noreply.github.com>
2023-07-18 19:12:09 -0500

    updates

--- Zero2Hero/1.Strings, Lists, and Numbers/1.numbers.py
+++ Zero2Hero/1.Strings, Lists, and Numbers/1.numbers.py
@@ -9,6 +9,7 @@
 % modulo or mod - Gives you the remainder of a division so 7 % 4 would return 3 as 4 goes into 7 one time with a remainder of 3
 ** to the power
 '''
+'''
 print('addition')
 print(2+1) # returns 3 or 2 + 1
 print('')
@@ -26,10 +27,13 @@ print(3%2) # returns 1 or 3 mod 2 as the remainder of 3 devided by 2 is 1
 print('')
 print('to the power')
 print(2**4) # returns 16 or 2 to the power of 4
+'''
 
 # Order of operations
+'''
 print('')
 print('OOO')
 print(2 + 10 * 10 + 3)
 #vs
 print((2 + 10) * (10 + 3))
+'''
